a4c20819888dc0ee2914ece3d87fa1fadd21ca32
Sergio-Mira <sergio.mira@swrve.com>
2014-10-28 18:15:42 +0000

    Rename problematic 'class' variable

--- Sdk/Track/SwrveSwizzleHelper.m
+++ Sdk/Track/SwrveSwizzleHelper.m
@@ -19,14 +19,14 @@ static Method _class_getInstanceMethodSelfOrParents(Class c, SEL selector) {
 // Replaces the selector on the class with the same selector on newObject.
 // Returns the implementation of the selector that was replaced, or NULL if
 // no replacement was done.
-+ (IMP) swizzleMethod:(SEL)selector inClass:(Class)class withImplementationIn:(NSObject*)newObject;
++ (IMP) swizzleMethod:(SEL)selector inClass:(Class)c withImplementationIn:(NSObject*)newObject;
 {
-    Method originalMethod = _class_getInstanceMethodSelfOrParents(class, selector);
+    Method originalMethod = _class_getInstanceMethodSelfOrParents(c, selector);
     IMP oldImplementation = method_getImplementation(originalMethod);
     
     Method newMethod = class_getInstanceMethod([newObject class], selector);
     if (!oldImplementation) {
-        class_addMethod(class, selector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod));
+        class_addMethod(c, selector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod));
     } else {
         IMP newImplementation = [newObject methodForSelector:selector];
         method_setImplementation(originalMethod, newImplementation);
@@ -35,9 +35,9 @@ static Method _class_getInstanceMethodSelfOrParents(Class c, SEL selector) {
     return NULL;
 }
 
-+ (void) deswizzleMethod:(SEL)selector inClass:(Class)class originalImplementation:(IMP)originalImplementation
++ (void) deswizzleMethod:(SEL)selector inClass:(Class)c originalImplementation:(IMP)originalImplementation
 {
-    Method originalMethod = class_getInstanceMethod(class, selector);
+    Method originalMethod = class_getInstanceMethod(c, selector);
     if (originalImplementation == NULL) {
         method_setImplementation(originalMethod, NULL);
     } else {
