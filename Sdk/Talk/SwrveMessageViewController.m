b47ef78e97ecbb37ee0fcb92f73010d161dd1137
Sergio-Mira <sergio.mira@swrve.com>
2015-03-18 10:47:43 +0000

    Added configurable background color for in-app messages, defaulted to transparent

--- Sdk/Talk/SwrveMessageViewController.m
+++ Sdk/Talk/SwrveMessageViewController.m
@@ -56,6 +56,11 @@
                                   thatDelegatesTo:self
                                          withSize:self.view.bounds.size
                                           rotated:false];
+        
+        // Update background color
+        if (current_format.backgroundColor != nil) {
+            self.view.backgroundColor = current_format.backgroundColor;
+        }
     } else {
         DebugLog(@"Couldn't find a format for message: %@", message.name);
     }
