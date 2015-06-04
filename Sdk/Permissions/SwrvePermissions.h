ca70e7fa5577a924a2c6d061b8a3ae7076704776
Sergio-Mira <sergio.mira@swrve.com>
2015-06-04 11:11:29 +0100

    Added permission filter check before displaying a conversation campaign

--- Sdk/Permissions/SwrvePermissions.h
+++ Sdk/Permissions/SwrvePermissions.h
@@ -7,12 +7,23 @@ static NSString* swrve_permission_status_unsupported = @"unsupported";
 static NSString* swrve_permission_status_denied = @"denied";
 static NSString* swrve_permission_status_authorized = @"authorized";
 
+static NSString* swrve_permission_status = @"swrve_permission_status";
+static NSString* swrve_permission_location_always = @"swrve.permission.ios.location.always";
+static NSString* swrve_permission_location_when_in_use = @"swrve.permission.ios.location.when_in_use";
+static NSString* swrve_permission_photos = @"swrve.permission.ios.photos";
+static NSString* swrve_permission_camera = @"swrve.permission.ios.camera";
+static NSString* swrve_permission_contacts = @"swrve.permission.ios.contacts";
+static NSString* swrve_permission_push_notifications = @"swrve.permission.ios.push_notifications";
+
+static NSString* swrve_permission_requestable = @".requestable";
+
 /*! Used internally to offer permission request support */
 @interface SwrvePermissions : NSObject
 
 + (BOOL)processPermissionRequest:(NSString*)action withSDK:(Swrve*)swrve;
 + (NSDictionary*) currentStatus;
 + (void)compareStatusAndQueueEventsWithSDK:(Swrve*)swrve;
++ (NSArray*) currentPermissionFilters;
 
 + (ISHPermissionState)checkLocationAlways;
 + (void)requestLocationAlways:(Swrve*)swrve;
