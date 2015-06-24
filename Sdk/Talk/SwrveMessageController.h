a1818a9bb1eaa6a5a6fdfea3f2f670c901da8d18
dominicmarmionswrve <dominic.marmion@swrve.com>
2015-06-24 16:21:29 +0100

    9192-increment_conversation_version

--- Sdk/Talk/SwrveMessageController.h
+++ Sdk/Talk/SwrveMessageController.h
@@ -1,6 +1,6 @@
 #import "SwrveMessageViewController.h"
 
-const static int CONVERSATION_VERSION        = 1;
+const static int CONVERSATION_VERSION        = 2;
 
 static NSString* const AUTOSHOW_AT_SESSION_START_TRIGGER = @"Swrve.Messages.showAtSessionStart";
 
