d4af3bebae5eabe65f71cbef5c08b2fd639990d5
dominicmarmionswrve <dominic.marmion@swrve.com>
2015-08-14 15:47:57 +0100

    capitalise swrve permission events

--- Sdk/Talk/SwrvePermissions.h
+++ Sdk/Talk/SwrvePermissions.h
@@ -2,20 +2,20 @@
 #import "Swrve.h"
 #import "ISHPermissionRequest.h"
 
-static NSString* swrve_permission_status_unknown = @"unknown";
-static NSString* swrve_permission_status_unsupported = @"unsupported";
-static NSString* swrve_permission_status_denied = @"denied";
-static NSString* swrve_permission_status_authorized = @"authorized";
+static NSString* swrve_permission_status_unknown        = @"unknown";
+static NSString* swrve_permission_status_unsupported    = @"unsupported";
+static NSString* swrve_permission_status_denied         = @"denied";
+static NSString* swrve_permission_status_authorized     = @"authorized";
+static NSString* swrve_permission_status                = @"swrve_permission_status";
 
-static NSString* swrve_permission_status = @"swrve_permission_status";
-static NSString* swrve_permission_location_always = @"swrve.permission.ios.location.always";
-static NSString* swrve_permission_location_when_in_use = @"swrve.permission.ios.location.when_in_use";
-static NSString* swrve_permission_photos = @"swrve.permission.ios.photos";
-static NSString* swrve_permission_camera = @"swrve.permission.ios.camera";
-static NSString* swrve_permission_contacts = @"swrve.permission.ios.contacts";
-static NSString* swrve_permission_push_notifications = @"swrve.permission.ios.push_notifications";
+static NSString* swrve_permission_location_always       = @"Swrve.permission.ios.location.always";
+static NSString* swrve_permission_location_when_in_use  = @"Swrve.permission.ios.location.when_in_use";
+static NSString* swrve_permission_photos                = @"Swrve.permission.ios.photos";
+static NSString* swrve_permission_camera                = @"Swrve.permission.ios.camera";
+static NSString* swrve_permission_contacts              = @"Swrve.permission.ios.contacts";
+static NSString* swrve_permission_push_notifications    = @"Swrve.permission.ios.push_notifications";
 
-static NSString* swrve_permission_requestable = @".requestable";
+static NSString* swrve_permission_requestable           = @".requestable";
 
 /*! Used internally to offer permission request support */
 @interface SwrvePermissions : NSObject
