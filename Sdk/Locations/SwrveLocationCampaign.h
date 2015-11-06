5e1f0d6489062b9b1896edbe667d195bec1c5a91
dominicmarmionswrve <dominic.marmion@swrve.com>
2015-11-06 16:13:01 +0000

    10359-remove_location_dates_from_ios_sdk

--- Sdk/Locations/SwrveLocationCampaign.h
+++ Sdk/Locations/SwrveLocationCampaign.h
@@ -4,8 +4,6 @@
 @interface SwrveLocationCampaign : NSObject
 
 @property(atomic, strong) NSString *locationCampaignId;
-@property(atomic, strong) NSDate *startDate;
-@property(atomic, strong) NSDate *endDate;
 @property(atomic, strong) NSNumber *version;
 @property(atomic, strong) SwrveLocationMessage *message;
 
