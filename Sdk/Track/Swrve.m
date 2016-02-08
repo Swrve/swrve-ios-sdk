94f294b25b4aa2bfc1da34edd0d3428e863a7709
dominicmarmionswrve <dominic.marmion@swrve.com>
2016-02-08 12:39:11 +0000

    11268-location_version_null

--- Sdk/Track/Swrve.m
+++ Sdk/Track/Swrve.m
@@ -706,6 +706,8 @@ static bool didSwizzle = false;
         blockStore = [[NSMutableDictionary alloc] init];
         blockStoreId = 0;
 
+        locationVersion = @"0";
+
         config = [[ImmutableSwrveConfig alloc] initWithSwrveConfig:swrveConfig];
         [self initBuffer];
         deviceInfo = [NSMutableDictionary dictionary];
