cac526a295741a9c6731396787fdfb97fdf2173a
seaders <seaders69@gmail.com>
2016-05-11 12:20:57 +0100

    Moving iOS Unity projects to Unity repo.

--- SwrveUnitySDK/UnitySDK/UnitySwrveCommonMessageController.h
+++
@@ -1,9 +0,0 @@
-#import "SwrveMessageEventHandler.h"
-#import "SwrveBaseConversation.h"
-
-@interface UnitySwrveMessageEventHandler : NSObject<SwrveMessageEventHandler>
-
--(SwrveBaseConversation*) conversationFromString:(NSString*)conversation;
--(void) showConversationFromString:(NSString*)conversation;
-
-@end
