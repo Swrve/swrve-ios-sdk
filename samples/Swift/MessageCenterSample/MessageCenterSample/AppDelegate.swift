214e6c536fd1972e42228e5cafabace977d70d96
Adam Govan <adam.govan@swrve.com>
2016-08-11 15:26:49 +0100

    Added  a Swift Sample Directory and a Swift Version of the Message Centre Sample Code.

---
+++ samples/Swift/MessageCenterSample/MessageCenterSample/AppDelegate.swift
@@ -0,0 +1,31 @@
+import UIKit
+import SwrveSDK
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        let config = SwrveConfig()
+        config.pushEnabled = true
+        config.prefersIAMStatusBarHidden = true
+        //TODO: change that to be generic
+        Swrve .sharedInstanceWithAppID(1030, apiKey: "SwrveDevApple", config: config)
+        
+        if let launchOptions = launchOptions {
+            let remoteNotification: [NSObject : AnyObject]? = launchOptions[UIApplicationLaunchOptionsRemoteNotificationKey] as! [NSObject : AnyObject]!
+            if (remoteNotification != nil) {
+                Swrve.sharedInstance().talk.pushNotificationReceived(remoteNotification)
+            }
+        }
+        
+        return true
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        Swrve.sharedInstance().talk.pushNotificationReceived(userInfo)
+    }
+
+}
