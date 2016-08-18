5d02835dad07a9a41fb9c6b246fe55ed07d33feb
Adam Govan <adam.govan@swrve.com>
2016-08-18 18:03:48 +0100

    updated the Sample projects to compile, but not run

--- samples/Swift/MessageCenterSample/MessageCenterSample/AppDelegate.swift
+++ samples/Swift/MessageCenterSample/MessageCenterSample/AppDelegate.swift
@@ -11,7 +11,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let config = SwrveConfig()
         config.pushEnabled = true
         config.prefersIAMStatusBarHidden = true
-        Swrve.sharedInstanceWithAppID(YOUR_APP_ID, apiKey: "YOUR_API_KEY", config: config)
+        
+        //FIXME: Add wour App ID (instead of -1) and your API Key (instead of <API_KEY>) here.
+        Swrve.sharedInstanceWithAppID(-1, apiKey: "<API_KEY>", config: config)
         
         if let launchOptions = launchOptions {
             let remoteNotification: [NSObject : AnyObject]? = launchOptions[UIApplicationLaunchOptionsRemoteNotificationKey] as! [NSObject : AnyObject]!
