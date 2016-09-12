7e50b3f5f2df73d784ccc4e877be43fbed12b7ef
Adam Govan <adam.govan@swrve.com>
2016-09-12 11:18:08 +0100

    updated the sample for Message Centre to Swift 3

--- samples/Swift/MessageCenterSample/MessageCenterSample/ViewController.swift
+++ samples/Swift/MessageCenterSample/MessageCenterSample/ViewController.swift
@@ -9,68 +9,68 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         refreshDataSource()
         self.tableView.reloadData()
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.newSwrveCampaigns(_:)), name:"SwrveUserResourcesUpdated", object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.newSwrveCampaigns(_:)), name:NSNotification.Name(rawValue: "SwrveUserResourcesUpdated"), object: nil)
     }
     
-    override func viewWillDisappear(animated: Bool) {
+    override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        NSNotificationCenter.defaultCenter().removeObserver(self)
+        NotificationCenter.default.removeObserver(self)
     }
     
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return campaigns.count
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("MessageCenterCell", forIndexPath: indexPath)
-        let baseCampaign = campaigns[indexPath.row] as? SwrveBaseCampaign
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCenterCell", for: indexPath)
+        let baseCampaign = campaigns[(indexPath as NSIndexPath).row] as? SwrveBaseCampaign
         cell.textLabel?.text = baseCampaign?.subject
         
-        let dformat : NSDateFormatter = NSDateFormatter()
+        let dformat : DateFormatter = DateFormatter()
         dformat.dateFormat = "MMMM dd, yyyy (EEEE) HH:mm:ss z Z"
-        cell.detailTextLabel?.text = dformat.stringFromDate((baseCampaign?.dateStart)!)
+        cell.detailTextLabel?.text = dformat.string(from: (baseCampaign?.dateStart)!)
         
         return cell
     }
     
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        Swrve.sharedInstance().talk.showMessageCenterCampaign(campaigns[indexPath.row] as? SwrveBaseCampaign)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        Swrve.sharedInstance().talk.showMessageCenter(campaigns[(indexPath as NSIndexPath).row] as? SwrveBaseCampaign)
     }
     
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     
-    override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
-        return UITableViewCellEditingStyle.Delete
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.delete
     }
     
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == UITableViewCellEditingStyle.Delete {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
             tableView.beginUpdates()
-            Swrve.sharedInstance().talk.removeMessageCenterCampaign(campaigns[indexPath.row] as? SwrveBaseCampaign)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Left)
+            Swrve.sharedInstance().talk.removeMessageCenter(campaigns[(indexPath as NSIndexPath).row] as? SwrveBaseCampaign)
+            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.left)
             tableView.endUpdates()
         }
     }
     
     func refreshDataSource() {
-        let unsortedCampaigns: NSArray = Swrve.sharedInstance().talk.messageCenterCampaigns()
+        let unsortedCampaigns: NSArray = Swrve.sharedInstance().talk.messageCenterCampaigns() as NSArray
         let descriptor: NSSortDescriptor = NSSortDescriptor(key: "dateStart", ascending: true)
-        campaigns = unsortedCampaigns.sortedArrayUsingDescriptors([descriptor])
+        campaigns = unsortedCampaigns.sortedArray(using: [descriptor]) as NSArray
     }
     
-    func newSwrveCampaigns(notification: NSNotification){
+    func newSwrveCampaigns(_ notification: Notification){
         refreshDataSource()
         self.tableView.reloadData()
     }
     
-    override func prefersStatusBarHidden() -> Bool {
+    override var prefersStatusBarHidden : Bool {
         return true
     }
 }
