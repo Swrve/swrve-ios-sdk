674a42cb6b1f24f3f41c2b0c8ab8bfa652335908
Milen Halachev <milenski88@gmail.com>
2017-01-03 16:12:39 +0200

    - added xcode project that contains a framework target

---
+++ SwrveSDK.h
@@ -0,0 +1,20 @@
+//
+//  SwrveSDK.h
+//  SwrveSDK
+//
+//  Created by Milen Halachev on 1/3/17.
+//  Copyright © 2017 Swrve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwrveSDK.
+FOUNDATION_EXPORT double SwrveSDKVersionNumber;
+
+//! Project version string for SwrveSDK.
+FOUNDATION_EXPORT const unsigned char SwrveSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwrveSDK/PublicHeader.h>
+
+
+#import <SwrveSDK/Swrve.h>
